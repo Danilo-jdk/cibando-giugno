65495fd9812e49e028b3673d1dadb4f6d1e00d44
Danilo-jdk <d.defilio@jdk.it>
2023-06-21 09:07:58 +0200

    prima lezione Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cibando-giugno';
+  nome = 'Danilo';
+  label = 'logo di cibando';
+
+  percorso = '../assets/images/imageBg-';
+  images = [
+    { id: 1, label: 'Spaghetti al pomodoro' },
+    // { id: 2, label: 'Tagliata di manzo' },
+    // { id: 3, label: 'Tiramisù classico' },
+  ];
 }
